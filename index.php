33f96c087460dbeec186c91040f1c0aba0e244e6
Nikola <nikola.flisik@poczta.onet.pl>
2023-03-11 23:46:14 +0100

    seks

---
+++ index.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        table {
+            border-spacing: 0;
+        }
+        td {
+            background-color: hotpink;
+            width: 20px;
+            height: 20px;
+        }
+    </style>
+</head>
+<body oncontextmenu="return false;">
+<script>
+    function click(element, button) {
+        console.log(2121)
+        if (button === 0) {
+            element.style.backgroundColor = 'pink'
+        }
+        else {
+            element.style.backgroundColor = 'hotpink'
+        }
+    }
+</script>
+    <table>
+        <tr>
+            <td onmousedown="window.click(this, event.button)"></td>
+            <td onmousedown="window.click(this, event.button)"></td>
+        </tr>
+        <tr>
+            <td onmousedown="window.click(this, event.button)"></td>
+            <td onmousedown="window.click(this, event.button)"></td>
+        </tr>
+    </table>
+
+</body>
+</html>
