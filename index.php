f54ba2e981ca3e24e51930eea4aca6e8c44ce248
szymusu <szymusu@gmail.com>
2023-03-24 00:25:39 +0100

    hold draw + bold

--- index.php
+++ index.php
@@ -17,27 +17,71 @@
         }
     </style>
 </head>
-<body oncontextmenu="return false;">
+<body oncontextmenu="return false" ondragstart="return false">
 <script>
-    function click(element, button) {
-        if (button === 0) {
-            element.style.backgroundColor = 'pink'
+
+    const mouseButtonState = [false, false, false]
+    let bold = true
+    let width
+
+    function squareMouseOver(element) {
+        const affected = [element]
+        if (bold) {
+            affected.push(document.getElementById((parseInt(element.id) - 1).toString()))
+            affected.push(document.getElementById((parseInt(element.id) + 1).toString()))
+            affected.push(document.getElementById((parseInt(element.id) - width).toString()))
+            affected.push(document.getElementById((parseInt(element.id) - width + 1).toString()))
+            affected.push(document.getElementById((parseInt(element.id) - width - 1).toString()))
+            affected.push(document.getElementById((parseInt(element.id) + width).toString()))
+            affected.push(document.getElementById((parseInt(element.id) + width + 1).toString()))
+            affected.push(document.getElementById((parseInt(element.id) + width - 1).toString()))
         }
-        else {
-            element.style.backgroundColor = 'hotpink'
+        const filtered = affected.filter((e) => !(parseInt(element.id) % width === width-1 && parseInt(e.id) % width === 0
+                              || parseInt(element.id) % width === 0 && parseInt(e.id) % width === width-1))
+
+        if (mouseButtonState[0]) {
+            filtered.forEach((e) => e.style.backgroundColor = "pink")
+        }
+        else if (mouseButtonState[2]) {
+            filtered.forEach((e) => e.style.backgroundColor = "hotpink")
         }
     }
-</script>
-    <table>
-        <tr>
-            <td onmousedown="window.click(this, event.button)"></td>
-            <td onmousedown="window.click(this, event.button)"></td>
-        </tr>
-        <tr>
-            <td onmousedown="window.click(this, event.button)"></td>
-            <td onmousedown="window.click(this, event.button)"></td>
-        </tr>
-    </table>
 
+    function createSquare(index) {
+        const element = document.createElement("td")
+        element.id = index
+        element.onmouseover = () => squareMouseOver(element)
+        return element
+    }
+
+    function createRow(length, rowIndex) {
+        const row = document.createElement("tr")
+        row.id = `row-${rowIndex}`
+        for (let i = 0; i < length; i++) {
+            row.appendChild(createSquare(rowIndex*length + i))
+        }
+        return row
+    }
+
+    function createTable(tableElement, rows, columns) {
+        width = columns
+        for (let i = 0; i < rows; i++) {
+            tableElement.appendChild(createRow(columns, i))
+        }
+    }
+
+    function main() {
+        const tableElement = document.querySelector("table#canvas")
+        createTable(tableElement, 50, 50)
+        tableElement.onmousedown = (event) => mouseButtonState[event.button] = true
+        tableElement.onmouseup = (event) => mouseButtonState[event.button] = false
+    }
+
+</script>
+    <table id="canvas"></table>
+
+    <script>
+        window.main()
+    </script>
 </body>
 </html>
