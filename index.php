6f66b3ed881973af03fa101eca2e2afac88bb92b
szymusu <szymusu@gmail.com>
2023-03-29 00:46:53 +0200

    colors, export to byte array

--- index.php
+++ index.php
@@ -2,18 +2,27 @@
 <html lang="pl">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>rysb.uk</title>
     <style>
-        table {
+        table#canvas {
             border-spacing: 0;
+            border: solid black 2px;
         }
         td {
-            background-color: hotpink;
             width: 20px;
             height: 20px;
+        }
+
+        .fl {
+            float: left;
+        }
+
+        .select-color {
+            width: 50px;
+            height: 50px;
+            margin: 10px;
         }
     </style>
 </head>
@@ -23,6 +32,9 @@
     const mouseButtonState = [false, false, false]
     let bold = true
     let width
+    let height
+    const colors = ["white", "black", "red", "blue", "green", "yellow"]
+    let colorIndex = 1
 
     function squareMouseOver(element) {
         const affected = [element]
@@ -40,10 +52,16 @@
                               || parseInt(element.id) % width === 0 && parseInt(e.id) % width === width-1))
 
         if (mouseButtonState[0]) {
-            filtered.forEach((e) => e.style.backgroundColor = "pink")
+            filtered.forEach((e) => {
+                e.style.backgroundColor = colors[colorIndex]
+                e.colorIndex = colorIndex
+            })
         }
         else if (mouseButtonState[2]) {
-            filtered.forEach((e) => e.style.backgroundColor = "hotpink")
+            filtered.forEach((e) => {
+                e.style.backgroundColor = colors[0]
+                e.colorIndex = 0
+            })
         }
     }
 
@@ -51,6 +69,8 @@
         const element = document.createElement("td")
         element.id = index
         element.onmouseover = () => squareMouseOver(element)
+        element.onmousedown = () => setTimeout(() => squareMouseOver(element), 1)
+        element.colorIndex = 0
         return element
     }
 
@@ -65,9 +85,34 @@
 
     function createTable(tableElement, rows, columns) {
         width = columns
+        height = rows
         for (let i = 0; i < rows; i++) {
             tableElement.appendChild(createRow(columns, i))
         }
+    }
+
+    function createSelectColor(index) {
+        const element = document.createElement("div")
+        element.className = "select-color"
+        element.style.backgroundColor = colors[index]
+        element.onclick = () => colorIndex = index
+        return element
+    }
+
+    function createColorPicker(colorPickerElement) {
+        for (let i = 1; i < colors.length; i++) {
+            colorPickerElement.appendChild(createSelectColor(i))
+        }
+    }
+
+    function encodeBoard() {
+        const array = new Uint8Array(width*height / 2)
+        for (let i = 0; i < width*height; i += 2) {
+            const leftNibble = document.getElementById(i.toString()).colorIndex
+            const rightNibble = document.getElementById((i+1).toString()).colorIndex
+            array[i/2] = leftNibble*16 + rightNibble
+        }
+        return array
     }
 
     function main() {
@@ -75,10 +120,14 @@
         createTable(tableElement, 50, 50)
         tableElement.onmousedown = (event) => mouseButtonState[event.button] = true
         tableElement.onmouseup = (event) => mouseButtonState[event.button] = false
+
+        const colorPickerElement = document.querySelector("div#color-picker")
+        createColorPicker(colorPickerElement)
     }
 
 </script>
-    <table id="canvas"></table>
+    <table id="canvas" class="fl"></table>
+    <div id="color-picker" class="fl"></div>
 
     <script>
         window.main()
